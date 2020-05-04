50346f89ab348657ef099273a4ed6c835ccb014b
PAVAN KUMAR REDDY TADI <pavankumart46@gmail.com>
2020-05-04 11:49:27 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -38,8 +38,7 @@ release = u'1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -48,7 +47,7 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
